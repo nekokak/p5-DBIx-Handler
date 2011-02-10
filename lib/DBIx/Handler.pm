aa7c14a01af3e355fd3725a98c70ed468d1afe31
Atsushi Kobayashi <nekokak@gmail.com>
2011-02-11 02:21:36 +0900

    initial import

---
+++ lib/DBIx/Handler.pm
@@ -0,0 +1,103 @@
+package DBIx::Handler;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+use DBI '1.613';
+
+sub new {
+    my $class = shift;
+
+    bless {
+        _connect_info => [@_],
+        _dbh          => undef,
+    }, $class;
+}
+
+sub _connect {
+    my $self = shift;
+
+    my $dbh = $self->{_dbh} = do {
+        if ($INC{'Apache/DBI.pm'} && $ENV{MOD_PERL}) {
+            local $DBI::connect_via = 'connect'; # Disable Apache::DBI.
+            DBI->connect( @{ $self->{_connect_info} } );
+        } else {
+            DBI->connect( @{ $self->{_connect_info} } );
+        }
+    };
+
+    if (@{ $self->{_connect_info} } < 4 || !exists $self->{_connect_info}[3]{AutoInactiveDestroy}) {
+        $dbh->STORE(AutoInactiveDestroy => 1);
+    }
+
+    if (@{ $self->{_connect_info} } < 4 || (!exists $self->{_connect_info}[3]{RaiseError} && !exists $self->{_connect_info}[3]{HandleError})) {
+        $dbh->STORE(RaiseError => 1);
+    }
+
+    $self->{_pid} = $$;
+
+    $dbh;
+}
+
+sub dbh {
+    my $self = shift;
+    $self->_seems_connected or $self->_connect;
+}
+
+sub _seems_connected {
+    my $self = shift;
+
+    my $dbh = $self->{_dbh} or return;
+
+    if ( $self->{_pid} != $$ ) {
+        $dbh->STORE(InactiveDestroy => 1);
+        return;
+    }
+
+    unless ($dbh->FETCH('Active') && $dbh->ping) {
+        return;
+    }
+
+    $dbh;
+}
+
+sub disconnect {
+    my $self = shift;
+
+    my $dbh = $self->{_dbh} or return;
+
+    $dbh->STORE(CachedKids => {});
+    $dbh->disconnect;
+    $self->{_dbh} = undef;
+}
+
+sub DESTROY { $_[0]->disconnect }
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Handler -
+
+=head1 SYNOPSIS
+
+  use DBIx::Handler;
+
+=head1 DESCRIPTION
+
+DBIx::Handler is
+
+=head1 AUTHOR
+
+Atsushi Kobayashi E<lt>nekokak _at_ gmail _dot_ comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
---
+++ t/01_basic.t
@@ -0,0 +1,51 @@
+use strict;
+use warnings;
+use DBIx::Handler;
+use Test::More;
+
+my $handler = DBIx::Handler->new('dbi:SQLite:','','');
+isa_ok $handler, 'DBIx::Handler';
+isa_ok $handler->dbh, 'DBI::db';
+
+subtest 'other db handler after disconnect' => sub {
+    my $dbh = $handler->dbh;
+
+    $handler->disconnect;
+
+    isnt $dbh, $handler->dbh;
+};
+
+subtest 'fork' => sub {
+
+    my $dbh = $handler->dbh;
+    if (fork) {
+        wait;
+        is $dbh, $handler->dbh;
+    } else {
+        isnt $dbh, $handler->dbh;
+        exit;
+    }
+};
+
+subtest 'no active handle case' => sub {
+    my $dbh = $handler->dbh;
+
+    $dbh->{Active} = 0;
+
+    isnt $dbh, $handler->dbh;
+};
+
+subtest 'can not ping case' => sub {
+
+    no strict 'refs';
+    no warnings 'redefine';
+
+    my $dbh = $handler->dbh;
+
+    my $ping = ref($handler->{_dbh}) . '::ping';
+    local *$ping = sub { 0 };
+
+    isnt $dbh, $handler->dbh;
+};
+
+done_testing;
